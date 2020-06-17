61b1ed477b503e73e29c3f3b20445a97d43aba4c
ssoraa <kiuy.or@icloud.com>
2020-06-17 11:17:19 +0200

    :pencil: create guild model

--- settings/configurations.example.js
+++ settings/configurations.example.js
@@ -3,5 +3,8 @@ module.exports = {
         name: 'hey',
         type: 'PLAYING' /** PLAYING / WATCHING / STREAMING / LISTING */,
         url: 'https://twitch.tv/ssoraa'
+    },
+    base: {
+        prefix: ','
     }
 };
--- utils/database/models.js
+++ utils/database/models.js
@@ -1,6 +1,22 @@
 module.exports = async (options) => {
     /** MODELS */
     try  {
+        options.database.define('guild', {
+            id: {
+                type: options.Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            guildID: {
+                type: options.Sequelize.STRING(18),
+                allowNull: false
+            },
+            prefix: {
+                type: options.Sequelize.STRING(5),
+                allowNull: false,
+                defaultValue: options.client.configuration.base.prefix
+            }
+        });
         await options.client.database.sync();
         return options.client.models;
     } catch (e) {
